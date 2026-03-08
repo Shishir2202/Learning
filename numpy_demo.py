fdfb5b1d1eff317c79a9ed745dedc9f42bac58a5
Shishir <Shrivastava.shishir28@gmail.com>
2026-03-08 13:27:25 +0530

    NumpyDemo

---
+++ numpy_demo.py
@@ -0,0 +1,88 @@
+import time
+import numpy as np
+
+# start_time= time.time()
+# arr1=[i for i in range(10000000)]
+# arr2=[i for i in range(10000000,20000000)]
+# arr3 = []
+#     for i in range(10000000):
+#     arr3.append(arr1[i]+arr2[i])
+# end_time = time.time()
+
+# print(end_time-start_time)
+
+# start_time= time.time()
+# arr1= np.arange(10000000)
+# arr2= np.arange(10000000,20000000)
+
+# arr3= arr1+arr2
+# end_time = time.time()
+
+# print(end_time-start_time)
+
+# arr1= np.array([1,2,3,2])
+# arr2= np.array([8,4,1])
+
+# result= arr1 + arr2
+
+# print(result)
+# print(type(result))
+
+# arr1=np.array([[1,2,3],[3,4,5],[5,6,7],[9,1,11]])
+# arr2=np.array([2,2])
+
+# result= arr1 + arr2
+
+# print(result)
+
+# arr1= np.array([
+#     [[1,2],[8,9]],
+#     [[3,4],[10,11]],
+#     [[5,6],[12,13]]
+#     ])
+
+# arr1= np.array([
+#     [[1,2],[8,9]],
+#     [[3,4],[10,11]],
+#     [[5,6],[12,13]]
+#     ])
+
+# arr2= [[1,3],[1,3]]
+
+# result= arr1 + arr2
+
+# print(result)
+
+# print(arr)
+# print(arr1.shape)
+
+# print(arr1.ndim)
+
+# print(arr.dtype)
+
+# arr1=np.array([[0,0],[0,0],[0,0],[0,0]])
+
+# print(arr1)
+
+# arr= np.zeros((10,4),dtype=int)
+# print(arr1)
+
+# print(arr.dtype)
+
+# print(arr1)
+# print(arr1[1][1:3])
+
+# print(arr1**2)
+
+#flatten - flatten the array
+
+
+# print(arr1.flatten())
+
+# print(arr1.ravel())
+
+# print(arr1)
+
+#raven - flatten but might change
+
+print(np.random.rand(2,5))
